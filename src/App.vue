b270480d75edc9d49083ba201ce901831dbf1ec9
reason <reasonz@daum.net>
2022-04-22 18:57:31 +0900

    Add my first Vue project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="nav-container">
+      <div class="nav">
+        <a class="logo" href="#">LOGO</a>
+        <ul class="nav-menu">
+          <li v-for="menuItem in menu" :key="menuItem">{{ menuItem }}</li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="header-bg">
+      <div class="blur-bg"></div>
+      <h1 class="header-text">REASON SHOP</h1>
+    </div>
+
+    <div>
+      <p class="text-add-item">신규 상품 추가</p>
+    </div>
+
+    <div class="item-container">
+      <div class="item-list" v-for="(item, i) in items" :key="i">
+        <ul class="item">
+          <img :src="require(`./assets/${item.img}.jpg`)" alt="" />
+          <li class="item-name">{{ item.title }}</li>
+          <li class="item-price">{{ item.price }}원</li>
+        </ul>
+        <button class="btn">구매하기</button>
+      </div>
+    </div>
+
+    <div style="height: 500px"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import item from './assets/product.js';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      menu: ['Home', 'About', 'menu3', 'menu4'],
+      items: item,
+    };
+  },
+  methods: {},
+};
 </script>
 
 <style>
@@ -21,6 +56,151 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body,
+h1,
+div,
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+li {
+  list-style: none;
+}
+
+.nav-container {
+  background: steelblue;
+  color: white;
+  margin: 0 auto;
+}
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.logo {
+  text-decoration: none;
+  font-size: 20px;
+  color: white;
+  margin-left: 20px;
+}
+
+.nav-menu {
+  display: flex;
+  align-items: center;
+}
+
+.nav-menu li {
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+.nav-menu li:last-child {
+  margin-right: 20px;
+}
+
+/* 최상단 메인 이미지*/
+.header-bg {
+  background: linear-gradient(rgba(54, 82, 154, 0.5), rgba(10, 12, 15, 0.5)), url(./assets/bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-blend-mode: darken;
+  width: 100%;
+  margin: 0 auto;
+  margin-bottom: 50px;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-position: center;
+  position: relative;
+}
+
+.blur-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(5px);
+  position: absolute;
+}
+
+.header-text {
+  color: #fff;
+  font-size: 50px;
+  z-index: 2;
+}
+/* 상품 위 텍스트 */
+.text-add-item {
+  font-size: 20px;
+  font-weight: 900;
+  border: none;
+  border-bottom: 2px solid #eee;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-bottom: 50px;
+  margin-bottom: 30px;
+}
+
+/* 상품들 */
+
+.item-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  max-width: 1200px;
+  gap: 30px;
+  margin: 0 auto;
+}
+
+.item-list {
+  padding: 10px;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  box-shadow: 5px 5px 7px #eee;
+  height: 100%;
+}
+
+.item {
+  width: 100%;
+  padding: 0;
+}
+
+.item img {
+  width: 100%;
+  display: block;
+  border-radius: 5px;
+  height: 100%;
+}
+
+.item-name {
+  font-size: 1.2rem;
+  font-weight: 900;
+  margin-top: 10px;
+  margin-bottom: 5px;
+}
+
+.item-price {
+  font-size: 1rem;
+  margin-bottom: 10px;
+}
+
+.btn {
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: #fff;
+  background: #2c3e50;
+  width: 100%;
 }
 </style>
---
+++ src/assets/product.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 0,
+    title: '하늘색 원피스',
+    price: 50000,
+    img: 'img0',
+  },
+  {
+    id: 1,
+    title: '차콜 롱코트',
+    price: 85000,
+    img: 'img1',
+  },
+  {
+    id: 2,
+    title: '청바지(연청)',
+    price: 23000,
+    img: 'img2',
+  },
+];
